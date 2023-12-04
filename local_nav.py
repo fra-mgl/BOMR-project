2e7f39c714b0ecc47b29f58718541aa91339e134
= <=>
2023-12-04 20:04:44 +0100

    Local_nav_update

--- local_nav.py
+++ local_nav.py
@@ -3,8 +3,9 @@ import tdmclient.notebook
 await tdmclient.notebook.start() 
 import time
 import math
-state = 0
+state = "begin"
 target = 0
+obstacle = False
 xi=0
 yi=0
 xt=0
@@ -107,10 +108,10 @@ def navigate_obstacle(target, sens, k):
         rotate_robot(90, 100, switch_sens(sens))
         forward_robot(100)
         rotate_robot(90, 100, sens)
-        state = 5
+        state = "orientation"
     else:
         k += 1
-        state = 5
+        state = "orientation"
     return k, state
 
 def handle_target_left():
@@ -119,7 +120,7 @@ def handle_target_left():
     forward_robot(100)
     rotate_robot(90, 100, 1)
     forward_robot(100)
-    state = 5
+    state = "orientation"
     return state
 
 def handle_target_right():
@@ -132,20 +133,20 @@ def handle_target_right():
     print("j'ai tourné")
     forward_robot(100)
     print("c'est fini")
-    state = 5
+    state = "orientation"
     return state
 
 def handle_target_state(path, k):
     if path[0][k] == path[0][0]:
         print("je fais rien")
-        state = 6
+        state = "end"
     elif path[0][0] > path[0][k]:
         rotate_robot(90, 100, 2)
-        state = 6
+        state = "end"
     elif path[0][0] < path[0][k]:
         print("je dois tourner")
         rotate_robot(90, 100, 1)
-        state = 6
+        state = "end"
     return state
 
 def stop_and_print():
@@ -153,7 +154,7 @@ def stop_and_print():
     forward_robot(0)
     
 def local_nav():
-    global prox_horizontal, state, carre, path, position_i, special_cases, case, position, target, k
+    global prox_horizontal, state, carre, path, position_i, special_cases, case, position, target, k, obstacle
     
     w_l = [0,  20, -20, -20, 0, 0, 0]
     w_r = [0, -20, -20,  20, 0, 0,  0]
@@ -169,133 +170,41 @@ def local_nav():
         # Get and scale inputs
         x[i] = proxi[i] // sensor_scale
         if (x[2] > 20):
-            state = 1
+            obstacle = True 
+    return obstacle 
     
-    
-    if(state == 0):
-        beginning_robot(100)
-    elif state == 1:
-        forward_robot(0) 
-        obstacle_position = path[0][0]
-        print(obstacle_position)
-        print(position[0])
-        if position[0] == obstacle_position:
-            target = handle_obstacle_behind(path, special_cases)
-            print(target)
-            state = 2
-        if (position[0] > path[0][0]):
-            handle_obstacle_left(position, obstacle_position)
-            state = 3
-        if (position[0] < path[0][0]):
-            handle_obstacle_right(position, obstacle_position)
-            state = 4
-    if state == 2:
+def obstacle_function():
+    global prox_horizontal, state, carre, path, position_i, special_cases, case, position, target, k, obstacle
+    forward_robot(0) 
+    obstacle_position = path[0][0]
+    print(obstacle_position)
+    print(position[0])
+    if position[0] == obstacle_position:
+        target = handle_obstacle_behind(path, special_cases)
+        print(target)
+        state = "obstacle_behind"
+    if (position[0] > path[0][0]):
+        handle_obstacle_left(position, obstacle_position)
+        state = "obstacle_left"
+    if (position[0] < path[0][0]):
+        handle_obstacle_right(position, obstacle_position)
+        state = "obstacle_right"
+    if state == "obstacle_behind":
         k, state = navigate_obstacle(target, 1, k)
-    if state == 3:
+    if state == "obstacle_left":
         state = handle_target_left()
-    if state == 4:
+    if state == "obstacle_right":
         state = handle_target_right()
-    if state == 5:
+    if state == "orientation":
         state = handle_target_state(path, k)
-    if state == 6:
-        stop_and_print()          
+    if state == "end":
+        stop_and_print() 
+        obstacle = False
+        k = 1
     
 start = 0 
-while start==0: 
-    local_nav()
-    
-    
-    y = [0,0]
-    x = [0,0,0,0,0,0,0]
-    proxi,=get_var("prox_horizontal")
-        
-    for i in range(len(x)):
-        # Get and scale inputs
-        x[i] = proxi[i] // sensor_scale
-        if (x[2] > 20):
-            state = obstacle
-          
-    if(state == begin):
-        #print("avance")
+while start==0:
+    if(local_nav()):
+        obstacle_function()
+    else :
         beginning_robot(100)
-    if(state == obstacle):
-        print("stop")
-        # le robot s'arrête devant l'obstacle pendant 2 secondes 
-        forward_robot(0)
-        print("1") 
-        # ils ont le même x donc le global path est derrière l'obstacle
-        if (position[0] == path[0][0]): 
-            for i in range(len(path)):
-                #for j in (len(special_cases)):
-                if(path[0][i]==special_cases[0][0]):
-                    target = 1
-                if(path[0][i]==special_cases[0][1]):
-                    target = 2
-            state = obstacle_derriere
-        # la target est à gauche de l'obstacle 
-        if (position[0] > path[0][0]): 
-            state = obstacle_left
-        # la target est à droite de l'obstacle 
-        if (position[0] < path[0][0]): 
-            state = obstacle_right
-    if(state == obstacle_derriere):
-        print("target derrière obstacle")
-        if (target == 1):
-            print("obstacle à droite")
-            sens = 1
-        else :
-            print("obstacle à gauche") 
-            sens = 2
-        rotate_robot(90,100,sens)
-        forward_robot(100)
-        rotate_robot(90,100,switch_sens(sens))
-        forward_robot(100)
-        time.sleep(5)
-        if(target == 0):
-            print("path est bien derriere")
-            rotate_robot(90,100,switch_sens(sens))
-            forward_robot(100)
-            rotate_robot(90,100,sens)
-            state = 5
-        else : 
-            i+=1
-            state = 5
-    if(state == obstacle_left):
-        print("target à gauche")
-        rotate_robot(90,100,2)
-        forward_robot(100)
-        rotate_robot(90,100,1)
-        forward_robot(100)
-        state = 5
-    if(state == obstacle_right):
-        print("target à droite")
-        rotate_robot(90,100,1)
-        print("jai tourné")
-        forward_robot(100)
-        print("jai avancé")
-        rotate_robot(90,100,2)
-        print("jai tourné")
-        forward_robot(100)
-        print("c'est fini")
-        state = next_step
-    if(state == next_step):
-        #if(path[0][1] == path[0][0]):
-        if(path[0][i] == path[0][0]):
-            print("je fais rien")
-            state = end
-        if(path[0][0] > path[0][i]):
-            rotate_robot(90,100,2)
-            state = end
-        if(path[0][0] < path[0][i]):
-            print("je dois tourner")
-            rotate_robot(90,100,1)
-            state = end
-    if(state == end):
-        print("stop")
-        forward_robot(0)
-        
-           
-    
-start = 0 
-while start==0: 
-    local_nav()
